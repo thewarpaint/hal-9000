907b52b41f1c66328885a0719f3cc257102c9c2c
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2020-10-10 12:13:21 -0500

    (bbva) Fix login logic

--- bbva.js
+++ bbva.js
@@ -14,11 +14,16 @@ const urls = {
 const selectors = {
   cardNumberInput: '#tempCuenta',
   loginBtn: '#loginButton',
+  loginIframe: '.access__iframe',
+  // index is 0-based, we convert to 1-based tabindex transparently
   otpInput: (index) => `.inputToken[tabindex="${index+1}"]`,
+  passwordInput: '#password',
   showLoginFormBtn: '.header__actions__ulist .header__actions__list:nth-child(2) .header__actions__item__link',
+  validateTokenBtn: '#envioToken',
 };
 
 // TODO: Improve signature
+// otp means "one time password"
 async function login(page, cardNumber, password, otp) {
   if (otp.length !== 8) {
     console.error(`otp length is ${otp.length}, expected 8 characters!`);
@@ -29,24 +34,35 @@ async function login(page, cardNumber, password, otp) {
 
   await page.waitForSelector(selectors.showLoginFormBtn, { visible: true });
   await page.click(selectors.showLoginFormBtn);
+  await page.waitForSelector(selectors.loginIframe, { visible: true });
 
-  await page.waitForSelector(selectors.cardNumberInput, { visible: true });
-  await page.type(selectors.cardNumberInput, cardNumber);
-  await page.click(selectors.loginBtn);
+  // The first login step happens inside an iframe
+  const $loginIframe = await page.$(selectors.loginIframe);
+  const loginContentFrame = await $loginIframe.contentFrame();
 
-  await page.waitForSelector(selectors.password, { visible: true });
-  await page.type(selectors.password, password);
+  await loginContentFrame.waitForSelector(selectors.cardNumberInput, { visible: true });
+  await loginContentFrame.type(selectors.cardNumberInput, cardNumber);
+  await loginContentFrame.click(selectors.loginBtn);
+
+  await page.waitForSelector(selectors.passwordInput, { visible: true });
+  await page.type(selectors.passwordInput, password);
+  // TODO: Double check if this is really necessary
+  await page.waitForTimeout(1000);
   await page.click(selectors.loginBtn);
 
   // Wait for the first OTP input to be visible
   await page.waitForSelector(selectors.otpInput(0), { visible: true });
 
-  await Promise.all(otp.split('').map(async (otpChar, i) => {
-    await page.type(selectors.otpInput(i), otpChar);
-  }));
+  const otpChars = otp.split('');
 
-  // TODO: Use correct selector
-  await page.click(selectors.loginBtn);
+  for (let i = 0; i < otpChars.length; i++) {
+    await page.waitForTimeout(100);
+    await page.type(selectors.otpInput(i), otpChars[i]);
+  }
+
+  await page.click(selectors.validateTokenBtn);
+
+  // TODO: Wait for an element to confirm login
 }
 
 // Session needs to be closed, otherwise you won't be able to log in again for ~15 mins
@@ -80,6 +96,9 @@ async function main() {
   const page = await browser.newPage();
 
   await login(page, BBVA_CARD_NUMBER, BBVA_PASSWORD, BBVA_OTP);
+
+  // TODO: Remove
+  await page.waitForTimeout(10000);
 }
 
 (async () => {
